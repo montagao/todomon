b2751891f9ce6502a1d3c6cc37094e96784925c7
nicelah <mzgao@uwaterloo.ca>
2023-11-06 08:32:59 +0800

    fix whitespace bug

--- lua/todomon/init.lua
+++ lua/todomon/init.lua
@@ -4,12 +4,17 @@ local M = {}
 -- Define the Lua function to toggle a to-do list item
 function M.toggle_todo()
     local line = vim.api.nvim_get_current_line()
-    if line:match("^%s*[%*%-]+%s*%[%s%]") then
-        line = line:gsub("%[%s%]", "[x]")
-    elseif line:match("^%s*[%*%-]+%s*%[x%]") then
-        line = line:gsub("%[x%]", "[ ] ")
+    -- Check if the line has an incomplete to-do list marker
+    if line:match("^%s*[%*%-]+%s*%[%s?%]") then
+        -- Toggle to completed, ensuring exactly one space inside the brackets
+        line = line:gsub("(%[)%s?(%])", "%1x%2")
+    -- Check if the line has a complete to-do list marker
+    elseif line:match("^%s*[%*%-]+%s*%[x%s?%]") then
+        -- Toggle to incomplete, ensuring exactly one space inside the brackets
+        line = line:gsub("(%[)x%s?(%])", "%1 %2")
     else
-        line = line:gsub("^([%s%*%-]+)", "%1[ ] ")
+        -- It's not a to-do, make it one, with one space inside the brackets
+        line = line:gsub("^([%s%*%-]+)", "%1[ ]")
     end
     vim.api.nvim_set_current_line(line)
 end
